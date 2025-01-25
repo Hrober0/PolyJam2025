6e950eab860ecd7488f4ce808c6744eab62a293f
BejPastan <s.synaszko@gmail.com>
2025-01-25 15:37:29 +0100

    Dash
    but better

--- Assets/Scripts/PowerUps/Dash.cs
+++ Assets/Scripts/PowerUps/Dash.cs
@@ -8,11 +8,13 @@ public class Dash : MonoBehaviour
     private const float DASH_TIME = 0.016f;
 
     [SerializeField]
-    private float dashRange = 5f;
+    private float dashRange = 10f;
+    Rigidbody rb;
 
     private void Start()
     {
         GetComponent<ActiveCall>().OnActiveCall += StartDash;
+        rb = GetComponent<Rigidbody>();
     }
 
     private void StartDash()
@@ -27,7 +29,7 @@ public class Dash : MonoBehaviour
 
         while (dashRange > 0.2f)
         {
-            transform.Translate(direction * DASH_TIME * dashRange * 4f, Space.World);
+            rb.MovePosition(transform.position + direction * DASH_TIME * dashRange * 4f);
             dashRange -= DASH_TIME * dashRange * 4f;
             await Awaitable.WaitForSecondsAsync(DASH_TIME);
         }
