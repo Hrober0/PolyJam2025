349182148303e07c1044ab5699bfdd868999703a
BejPastan <s.synaszko@gmail.com>
2025-01-26 11:44:49 +0100

    change bubble model

--- Assets/Scripts/Bubble/BubbleInteractionChandler.cs
+++ Assets/Scripts/Bubble/BubbleInteractionChandler.cs
@@ -4,11 +4,22 @@ public class BubbleInteractionChandler : MonoBehaviour
 {
     [SerializeField] private Bubble bubble;
 
-    private void OnTriggerEnter(Collider other)
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    var player = other.GetComponentInParent<Player>();
+    //    if (player != null)
+    //    {
+    //        Debug.Log("huj2");
+    //        bubble.SetPlayer(player.Data);
+    //    }
+    //}
+
+    private void OnCollisionEnter(Collision collision)
     {
-        var player = other.GetComponentInParent<Player>();
+        var player = collision.collider.GetComponentInParent<Player>();
         if (player != null)
         {
+            Debug.Log("huj");
             bubble.SetPlayer(player.Data);
         }
     }
