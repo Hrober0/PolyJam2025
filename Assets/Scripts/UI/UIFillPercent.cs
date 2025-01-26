381efaff8a8494d812e19e4f1137da9a3ddd318f
ikcikoR <ikcikor@gmail.com>
2025-01-26 02:44:02 +0100

    Work on scoreboard

--- Assets/Scripts/UI/UIFillPercent.cs
+++ Assets/Scripts/UI/UIFillPercent.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UIFillPercent : MonoBehaviour
@@ -19,5 +22,15 @@ public class UIFillPercent : MonoBehaviour
     private void UpdateScore()
     {
         label.text = $"Fill percent: {Mathf.RoundToInt(FloorPainter.Instance.FillPercent * 100)}%";
+        var scoresCopy = new Dictionary<int,int>(FloorPainter.Instance.playerScores);
+        scoresCopy.OrderBy((kvp) => kvp.Value);
+        while(scoresCopy.Count>0) {
+            var record = scoresCopy.Last();
+            scoresCopy.Remove(record.Key);
+            if(record.Key == -1){
+                continue;
+            }
+            label.text += $"\nPlayer {record.Key+1}'s score: {Mathf.RoundToInt(record.Value/(float)FloorPainter.Instance.txtValues.Length*100)}%";
+        }
     }
 }
